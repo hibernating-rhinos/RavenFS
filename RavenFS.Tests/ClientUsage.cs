3189fb2f3b6fb1215b0f3b34bea3db76e776d5da
Ayende Rahien <ayende@ayende.com>
2011-08-01 14:25:31 +0300

    Adding client tests - Failing test for resuming

---
+++ RavenFS.Tests/ClientUsage.cs
@@ -0,0 +1,72 @@
+using System.IO;
+using RavenFS.Client;
+using Xunit;
+
+namespace RavenFS.Tests
+{
+	public class ClientUsage : ServerTest
+	{
+		private readonly RavenClient client = new RavenClient("http://localhost:9090");
+
+		[Fact]
+		public void CanUpload()
+		{
+			var ms = new MemoryStream();
+			var streamWriter = new StreamWriter(ms);
+			var expected = new string('a',1024);
+			streamWriter.Write(expected);
+			streamWriter.Flush();
+			ms.Position = 0;
+
+			client.Upload("abc.txt", ms).Wait();
+
+			Assert.Equal(expected, webClient.DownloadString("/files/abc.txt"));
+		}
+
+		[Fact]
+		public void CanDownload()
+		{
+			var ms = new MemoryStream();
+			var streamWriter = new StreamWriter(ms);
+			var expected = new string('a', 1024);
+			streamWriter.Write(expected);
+			streamWriter.Flush();
+			ms.Position = 0;
+
+			client.Upload("abc.txt", ms).Wait();
+
+			var ms2 = new MemoryStream();
+			client.Download("abc.txt", ms2).Wait();
+
+			ms2.Position = 0;
+
+			var actual = new StreamReader(ms2).ReadToEnd();
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void CanDownloadPartial()
+		{
+			var ms = new MemoryStream();
+			var streamWriter = new StreamWriter(ms);
+			var expected = new string('a', 2048);
+			streamWriter.Write(expected);
+			streamWriter.Flush();
+			ms.Position = 0;
+
+			client.Upload("abc.txt", ms).Wait();
+
+			var ms2 = new MemoryStream();
+			streamWriter = new StreamWriter(ms2);
+			streamWriter.Write(new string('a', 1024));
+			streamWriter.Flush();
+			
+			client.Download("abc.txt", ms2).Wait();
+
+			var actual = new StreamReader(ms2).ReadToEnd();
+
+			Assert.Equal(new string('a', 2048), actual);
+		}
+	}
+}
