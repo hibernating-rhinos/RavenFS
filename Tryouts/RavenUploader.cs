22cdd3089e8ee24ed00247ea4cbd6d4e4e3151eb
Ayende Rahien <ayende@ayende.com>
2011-08-23 19:45:04 +0300

    Sample code showing how to upload directly from the web without buffers

---
+++ Tryouts/RavenUploader.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Web;
+using RavenFS.Client;
+
+namespace Tryouts
+{
+	public class RavenUploaderHandler: IHttpHandler
+	{
+		RavenFileSystemClient client = new RavenFileSystemClient("http://localhost/ravenfs");
+
+		public void ProcessRequest(HttpContext context)
+		{
+			var parser = new MultiPartParser(context.Request.GetBufferlessInputStream());
+			Tuple<Stream, NameValueCollection> tuple;
+			while((tuple = parser.Next()) != null)
+			{
+				var upload = client.Upload(tuple.Item2["Content-Disposition"], tuple.Item2, tuple.Item1);
+				upload.Wait();// we have to wait here because we are reading from a single stream
+			}
+		}
+
+		public bool IsReusable
+		{
+			get { return true; }
+		}
+	}
+}
