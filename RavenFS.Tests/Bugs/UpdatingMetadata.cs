d1bc8f2d510470ec114e5523d6c3f111e56a57c3
Ayende Rahien <ayende@ayende.com>
2011-11-09 19:23:40 -0500

    Adding a test to make sure that we can update metadata from the client

---
+++ RavenFS.Tests/Bugs/UpdatingMetadata.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Specialized;
+using System.IO;
+using RavenFS.Client;
+using Xunit;
+
+namespace RavenFS.Tests.Bugs
+{
+	public class UpdatingMetadata : ServerTest
+	{
+		private readonly RavenFileSystemClient client = new RavenFileSystemClient("http://localhost:9090");
+
+
+		[Fact]
+		public void CanUpdateMetadata()
+		{
+			var ms = new MemoryStream();
+			var streamWriter = new StreamWriter(ms);
+			var expected = new string('a', 1024);
+			streamWriter.Write(expected);
+			streamWriter.Flush();
+			ms.Position = 0;
+
+			client.Upload("abc.txt", new NameValueCollection
+			{
+				{"test", "1"}
+			}, ms).Wait();
+
+			client.UpdateMetadata("abc.txt", new NameValueCollection
+			{
+				{"test", "2"}
+			});
+
+			var metadataFor = client.GetMetadataFor("abc.txt");
+
+
+			Assert.Equal("2", metadataFor.Result["test"]);
+		}
+
+		 
+	}
+}
