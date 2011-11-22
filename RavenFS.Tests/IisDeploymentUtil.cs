23c97892b25987fc2fd0cb2321084d12ab483a34
Ayende Rahien <ayende@ayende.com>
2011-11-22 18:07:36 +0200

    Moving to IIS Express for testing

---
+++ RavenFS.Tests/IisDeploymentUtil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using Raven.Database.Extensions;
+
+namespace RavenFS.Tests
+{
+	public class IisDeploymentUtil
+	{
+		protected const string WebDirectory = @".\RavenIISTestWeb\";
+
+		public static string DeployWebProjectToTestDirectory()
+		{
+			var fullPath = Path.GetFullPath(WebDirectory);
+			if (Directory.Exists(fullPath))
+			{
+				IOExtensions.DeleteDirectory(fullPath);
+			}
+
+			IOExtensions.CopyDirectory(GetRavenWebSource(), WebDirectory);
+
+			IOExtensions.DeleteDirectory(Path.Combine(fullPath, "Data.ravenfs"));
+			IOExtensions.DeleteDirectory(Path.Combine(fullPath, "Index.ravenfs"));
+
+			return fullPath;
+		}
+
+		private static string GetRavenWebSource()
+		{
+			foreach (var path in new[] { @"../../../RavenFS" })
+			{
+				var fullPath = Path.GetFullPath(path);
+
+				if (Directory.Exists(fullPath) && File.Exists(Path.Combine(fullPath, "web.config")))
+				{
+					var combine = Path.Combine(fullPath, "bin");
+					if (!Directory.Exists(combine) || Directory.GetFiles(combine, "RavenFS.dll").Length == 0)
+						throw new Exception("RavenFS\\bin at " + fullPath + " was nonexistant or empty, you need to build RavenFS.");
+
+					return fullPath;
+				}
+			}
+
+			throw new FileNotFoundException("Could not find source directory for RavenFS");
+		}
+	}
+}
---
+++ RavenFS.Tests/IisExpressTestClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using RavenFS.Client;
+
+namespace RavenFS.Tests
+{
+	public class IisExpressTestClient : IDisposable
+	{
+		public static int Port = 8084;
+
+		private IisExpressDriver _iisExpress;
+
+		protected WebClient webClient;
+
+		static IisExpressTestClient()
+		{
+			try
+			{
+				new Uri("http://localhost/?query=Customer:Northwind%20AND%20Preferred:True");
+			}
+			catch (Exception)
+			{
+			}
+		}
+
+		protected HttpWebRequest CreateWebRequest(string url)
+		{
+			return (HttpWebRequest)WebRequest.Create(_iisExpress.Url + url);
+		}
+
+		protected RavenFileSystemClient NewClient()
+		{
+			return new RavenFileSystemClient(_iisExpress.Url);
+		}
+
+		#region IDisposable Members
+
+		public void Dispose()
+		{
+			if (_iisExpress != null)
+			{
+				_iisExpress.Dispose();
+				_iisExpress = null;
+			}
+		}
+
+		#endregion
+
+		public IisExpressTestClient()
+		{
+			_iisExpress = new IisExpressDriver();
+			_iisExpress.Start(IisDeploymentUtil.DeployWebProjectToTestDirectory(), 8084);
+			webClient = new WebClient
+			{
+				BaseAddress = _iisExpress.Url
+			};
+		}
+	}
+}
