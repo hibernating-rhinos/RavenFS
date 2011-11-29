a74bb4129b02e6d76b1c9f91d49003877aea3cfa
Ayende Rahien <ayende@ayende.com>
2011-11-29 13:56:27 +0200

    Minor

--- Tryouts/Program.cs
+++ Tryouts/Program.cs
@@ -12,33 +12,9 @@ namespace Tryouts
 		{
 			var fs = new RavenFileSystemClient("http://localhost");
 
-			var ms = new MemoryStream();
-			var streamWriter = new StreamWriter(ms);
-			var expected = new string('a', 1024 * 1024 * 500);
-			streamWriter.Write(expected);
-			streamWriter.Flush();
-			ms.Position = 0;
-
-			Console.WriteLine("Writing...");
-
-			try
+			using(var f = File.OpenRead(@"C:\Users\Ayende\Downloads\Rhino.ServiceBus.dll"))
 			{
-				fs.UploadAsync("large-file-100mb", new NameValueCollection(), ms, (s, written) => Console.WriteLine("{0:#,#} kb", written/1024)).Wait();
-			}
-			catch (AggregateException e)
-			{
-				while(e.InnerException is AggregateException)
-				{
-					e = (AggregateException) e.InnerException;
-				}
-				var we = e.InnerException as WebException;
-				if (we == null)
-					throw;
-				var httpWebResponse = we.Response as HttpWebResponse;
-				if (httpWebResponse == null)
-					throw;
-
-				Console.WriteLine(new StreamReader(httpWebResponse.GetResponseStream()).ReadToEnd());
+				fs.UploadAsync("Rhino.ServiceBus.dll", f).Wait();
 			}
 		}
 		
