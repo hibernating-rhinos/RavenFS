0b31528a359dfcab36b6cea20be776a5be1b3495
Bozydar Sobczak <bozydar@ais.pl>
2012-02-01 10:58:36 +0100

    Rdc: External project added

---
+++ RavenFS/Handlers/SyncVersionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using RavenFS.Infrastructure;
+
+namespace RavenFS.Handlers
+{
+    [HandlerMetadata("^/sync/version", "GET")]
+    public class SyncVersionHandler : AbstractAsyncHandler
+    {
+        protected override Task ProcessRequestAsync(HttpContext context)
+        {
+            return WriteJson(context, new
+            {
+                Version = 1,
+            });
+        }
+    }
+}
