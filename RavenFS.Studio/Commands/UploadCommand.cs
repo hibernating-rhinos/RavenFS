0837f9cb542e630f51de448cf01b292837957fa0
samuel_d_jack <sam@seaturtlesoftware.com>
2012-03-01 17:15:24 +0000

    Extended Upload to allow selection of multiple files.
    Implemented Drag and Drop onto the File list to upload files.

--- RavenFS.Studio/Commands/UploadCommand.cs
+++ RavenFS.Studio/Commands/UploadCommand.cs
@@ -1,8 +1,11 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
 using RavenFS.Client;
 using RavenFS.Studio.Infrastructure;
 using RavenFS.Studio.Models;
 using RavenFS.Studio.Extensions;
+using FileInfo = System.IO.FileInfo;
 
 namespace RavenFS.Studio.Commands
 {
@@ -14,40 +17,61 @@ namespace RavenFS.Studio.Commands
 
 		public override void Execute(object parameter)
 		{
-			var fileDialog = new OpenFileDialog();
-			var result = fileDialog.ShowDialog();
-			if (result != true)
-				return;
+		    var files = parameter as IList<FileInfo>;
+            
+            if (files == null)
+            {
+                var fileDialog = new OpenFileDialog()
+                                     {
+                                         Multiselect = true
+                                     };
+                var result = fileDialog.ShowDialog();
+                if (result.HasValue && result.Value)
+                {
+                    files = fileDialog.Files.ToList();
+                }
+            }
 
-			var stream = fileDialog.File.OpenRead();
-			var fileSize = stream.Length;
-
-		    var filename = fileDialog.File.Name;
-		    var operation = new AsyncOperationModel()
-		                        {
-		                            Name = "Uploading " + filename,
-		                        };
-
-		    ApplicationModel.Current.Client.UploadAsync(
-		        filename,
-		        new NameValueCollection(),
-		        stream,
-		        (file, bytesUploaded) => operation.ProgressChanged(bytesUploaded,fileSize))
-		        .ContinueWith(task =>
-		                          {
-		                              stream.Dispose();
-
-                                      if (task.IsFaulted)
-                                      {
-                                          operation.Faulted(task.Exception.ExtractSingleInnerException());
-                                      }
-                                      else
-                                      {
-                                          operation.Completed();
-                                      }
-		                          });
-
-            ApplicationModel.Current.AsyncOperations.RegisterOperation(operation);
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    UploadFile(file);
+                }
+            }
 		}
+
+	    private static void UploadFile(FileInfo file)
+	    {
+	        var stream = file.OpenRead();
+	        var fileSize = stream.Length;
+
+	        var filename = file.Name;
+	        var operation = new AsyncOperationModel()
+	                            {
+	                                Name = "Uploading " + filename,
+	                            };
+
+	        ApplicationModel.Current.Client.UploadAsync(
+	            filename,
+	            new NameValueCollection(),
+	            stream,
+	            (fileName, bytesUploaded) => operation.ProgressChanged(bytesUploaded, fileSize))
+	            .ContinueWith(task =>
+	                              {
+	                                  stream.Dispose();
+
+	                                  if (task.IsFaulted)
+	                                  {
+	                                      operation.Faulted(task.Exception.ExtractSingleInnerException());
+	                                  }
+	                                  else
+	                                  {
+	                                      operation.Completed();
+	                                  }
+	                              });
+
+	        ApplicationModel.Current.AsyncOperations.RegisterOperation(operation);
+	    }
 	}
 }
