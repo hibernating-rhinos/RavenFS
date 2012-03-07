50620f705a3c981fff932212ed830480f57c3d4d
Ayende Rahien <ayende@ayende.com>
2012-03-07 15:50:14 +0200

    Moving RDC files to RavenFS, rather than an external project

--- Tryouts/Program.cs
+++ Tryouts/Program.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Specialized;
-using System.IO;
-using System.Net;
-using System.Threading;
-using RavenFS.Client;
-using RavenFS.Rdc.Wrapper;
 
 namespace Tryouts
 {
@@ -13,7 +7,6 @@ namespace Tryouts
 		[STAThread]
 		static void Main(string[] args)
 		{
-			IRdcLibrary x = (IRdcLibrary)new RdcLibrary();
 		}
 		
 	}
