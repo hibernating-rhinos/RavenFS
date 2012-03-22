98cd7658a99e348f2a32fec9ae3d28804f3cfa9d
Ayende Rahien <ayende@ayende.com>
2012-03-22 17:27:24 +0200

    Simplifying

--- RavenFS/Notifications/NotificationEndpoint.cs
+++ RavenFS/Notifications/NotificationEndpoint.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using SignalR;
+﻿using SignalR;
 
 namespace RavenFS.Notifications
 {
     public class NotificationEndpoint : PersistentConnection
     {
-        public NotificationEndpoint()
-        {
-        }
-
-        protected override System.Threading.Tasks.Task OnReceivedAsync(string connectionId, string data)
-        {
-           return base.OnReceivedAsync(connectionId, data);
-        }
     }
 }
