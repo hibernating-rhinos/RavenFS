7fa7bffc6208835e3bc6b3c837e3203860aa7e0b
Ayende Rahien <ayende@ayende.com>
2012-04-04 11:41:37 +0300

    Fixing an issue with deleting then renaming

--- RavenFS/Storage/StorageActionsAccessor.cs
+++ RavenFS/Storage/StorageActionsAccessor.cs
@@ -352,13 +352,14 @@ namespace RavenFS.Storage
 			if (!Api.TrySeek(session, Usage, SeekGrbit.SeekGE))
 				return;
 
-			Api.MakeKey(session, Usage, filename, Encoding.Unicode, MakeKeyGrbit.NewKey);
-			Api.JetSetIndexRange(session, Usage, SetIndexRangeGrbit.RangeInclusive);
-
 			Api.JetSetCurrentIndex(session, Pages, "by_id");
 
 			do
 			{
+				var rowName = Api.RetrieveColumnAsString(session, Usage, tableColumnsCache.UsageColumns["name"]);
+				if (rowName != filename)
+					break;
+
 				var pageId = Api.RetrieveColumnAsInt32(session, Usage, tableColumnsCache.UsageColumns["page_id"]).Value;
 
 				Api.MakeKey(session, Pages, pageId, MakeKeyGrbit.NewKey);
