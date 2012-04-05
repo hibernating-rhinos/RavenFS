75f950dc2d55a2fed17b3cb684f5dbea8967f33a
samuel_d_jack <sam@seaturtlesoftware.com>
2012-04-05 17:39:07 +0100

    Implemented Case #15: Files DataGrid now has a context menu for delete, rename, etc.

---
+++ RavenFS.Studio/Behaviors/SelectRowOnRightClickBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace RavenFS.Studio.Behaviors
+{
+    public class SelectRowOnRightClickBehavior : Behavior<DataGrid>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.MouseRightButtonDown += HandleRightButtonClick;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.MouseRightButtonDown += HandleRightButtonClick;
+        }
+
+        private void HandleRightButtonClick(object sender, MouseButtonEventArgs e)
+        {
+            var elementsUnderMouse = VisualTreeHelper.FindElementsInHostCoordinates(e.GetPosition(null), AssociatedObject);
+
+            var row = elementsUnderMouse
+                .OfType<DataGridRow>()
+                .FirstOrDefault();
+
+            if (row != null)
+                AssociatedObject.SelectedItem = row.DataContext;
+        }
+    }
+}
