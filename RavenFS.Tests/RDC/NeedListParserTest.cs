4eaef0d863e17181381a254dd6b9e07f2741888b
Bozydar Sobczak <bozydar@ais.pl>
2012-05-17 15:09:53 +0200

    Rdc: NeedListParser - refactored

---
+++ RavenFS.Tests/RDC/NeedListParserTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RavenFS.Rdc;
+using RavenFS.Rdc.Wrapper;
+using Xunit;
+
+namespace RavenFS.Tests.RDC
+{
+    public class NeedListParserTest
+    {
+        private class PartialDataAccessMock : IPartialDataAccess
+        {
+            private readonly string _name;
+            private readonly ConcurrentStack<string> _stack;
+
+            public PartialDataAccessMock(string name, ConcurrentStack<string> stack)
+            {
+                _name = name;
+                _stack = stack;
+            }
+
+            public Task CopyToAsync(Stream target, long from, long length)
+            {
+                return Task.Factory.StartNew(
+                    () =>
+                    {
+                        Thread.Sleep(new Random().Next(200));
+                        _stack.Push(_name);
+                    });
+            }
+        }
+
+        [Fact]
+        public void Should_call_source_and_seed_methods_asynchronously_but_in_the_proper_order()
+        {
+            var stack = new ConcurrentStack<string>();
+
+            const int needListSize = 100;
+            var partialDataAccessSourceMock = new PartialDataAccessMock("source", stack);
+            var partialDataAccessSeedMock = new PartialDataAccessMock("seed", stack);
+            var result = NeedListParser.ParseAsync(partialDataAccessSourceMock, partialDataAccessSeedMock,
+                                                   Stream.Null, GenerateAlternatedNeeds(needListSize));
+            result.Wait();
+            var calls = stack.Reverse().ToList();
+            for (var i = 0; i < needListSize; i++)
+            {
+                var s = i%2 == 0 ? "seed" : "source";
+                Assert.Equal(s, calls[i]);
+            }
+        }
+
+        private static IEnumerable<RdcNeed> GenerateAlternatedNeeds(int numberOfItems)
+        {
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                var need = new RdcNeed();
+                need.BlockLength = 10;
+                if (i % 2 == 0)
+                {
+                    need.BlockType = RdcNeedType.Seed;
+                }
+                else
+                {
+                    need.BlockType = RdcNeedType.Source;
+                }
+                yield return need;
+            }
+        }
+    }
+}
--- RavenFS/Rdc/NeedListParser.cs
+++ RavenFS/Rdc/NeedListParser.cs
@@ -2,54 +2,67 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using NLog;
 using RavenFS.Infrastructure;
 using RavenFS.Rdc.Wrapper;
+using System.Linq;
 
 namespace RavenFS.Rdc
 {
-	public static class NeedListParser
-	{
-        private static readonly Logger log = LogManager.GetCurrentClassLogger();
 
-		public static Task ParseAsync(IPartialDataAccess source, IPartialDataAccess seed, Stream output,
-		                              IList<RdcNeed> needList)
-		{
-		    var tcs = new TaskCompletionSource<object>();
+    public class NeedListParser
+    {
+        private TaskCompletionSource<object> _tcs;
 
-            // TODO: This code is causing a Stack Over Flow Exception, not sure how to fix this, so this is a workaround for now
+        public static Task ParseAsync(IPartialDataAccess source, IPartialDataAccess seed, Stream output, IEnumerable<RdcNeed> needList)
+        {
+            var me = new NeedListParser();
+            return me.ParseAsyncInternal(source, seed, output, needList);
+        }
 
-		    //Task.Factory.StartNew(() =>
-		    //                          {
-		                                  try
-		                                  {
-                                              foreach (var item in needList)
-                                              {
-                                                  switch (item.BlockType)
-                                                  {
-                                                      case RdcNeedType.Source:
-                                                          source.CopyToAsync(output, Convert.ToInt64(item.FileOffset),
-                                                                             Convert.ToInt64(item.BlockLength)).Wait();
-                                                          break;
-                                                      case RdcNeedType.Seed:
-                                                          seed.CopyToAsync(output, Convert.ToInt64(item.FileOffset),
-                                                                           Convert.ToInt64(item.BlockLength)).Wait();
-                                                          break;
-                                                      default:
-                                                          throw new NotSupportedException();
-                                                  }
-                                              }
+        private NeedListParser()
+        {
+        }
 
-		                                      tcs.TrySetResult(null);
-		                                  }
-		                                  catch (Exception e)
-		                                  {
-                                              log.ErrorException("Error during need list parsing", e);
-		                                      tcs.TrySetException(e);
-		                                  }
-		      //                        });
+        private Task ParseAsyncInternal(IPartialDataAccess source, IPartialDataAccess seed, Stream output,
+                                      IEnumerable<RdcNeed> needList)
+        {
+            _tcs = new TaskCompletionSource<object>();
+            var needListNullable = from item in needList select (RdcNeed?)item;
+            ParseAsyncInternal(source, seed, output, needListNullable);
+            return _tcs.Task;
+        }
 
-		    return tcs.Task;
-		}
-	}
+        private void ParseAsyncInternal(IPartialDataAccess source, IPartialDataAccess seed, Stream output,
+                                      IEnumerable<RdcNeed?> needList)
+        {
+            var itemNullable = needList.FirstOrDefault();
+            if (itemNullable == null)
+            {
+                _tcs.SetResult(null);
+            }
+            else
+            {
+                var item = itemNullable.Value;
+                var tail = needList.Skip(1);
+                var newTask = new Func<Task>(
+                    () =>
+                    {
+                        switch (item.BlockType)
+                        {
+                            case RdcNeedType.Source:
+                                return source.CopyToAsync(output,
+                                                          Convert.ToInt64(item.FileOffset),
+                                                          Convert.ToInt64(item.BlockLength));
+                            case RdcNeedType.Seed:
+                                return seed.CopyToAsync(output,
+                                                        Convert.ToInt64(item.FileOffset),
+                                                        Convert.ToInt64(item.BlockLength));
+                            default:
+                                throw new NotSupportedException();
+                        }
+                    });
+                newTask().ContinueWith(_ => ParseAsyncInternal(source, seed, output, tail));
+            }
+        }
+    }
 }
