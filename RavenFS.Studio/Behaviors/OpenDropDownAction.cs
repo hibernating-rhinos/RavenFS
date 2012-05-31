f44568508a9733841f4b1e372cbe213dc788864f
samuel_d_jack <sam@seaturtlesoftware.com>
2012-05-31 13:05:03 +0100

    Search window: Made the list of Meta-data keys pop up as soon as the clause builder is clicked.

---
+++ RavenFS.Studio/Behaviors/OpenDropDownAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace RavenFS.Studio.Behaviors
+{
+    public class OpenDropDownAction : TriggerAction<AutoCompleteBox>
+    {
+        protected override void Invoke(object parameter)
+        {
+            Dispatcher.BeginInvoke(() => AssociatedObject.IsDropDownOpen = true);
+        }
+    }
+}
