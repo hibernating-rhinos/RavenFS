a3da6838497c21a4a794fecaab2f217eaf0f52fe
Arkadiusz Palinski <arek@ais.pl>
2012-07-13 12:29:32 +0200

    Fixed unchecked condition of file name if there is the same work in a queue but with a lower ETag

--- RavenFS/Synchronization/SynchronizationQueue.cs
+++ RavenFS/Synchronization/SynchronizationQueue.cs
@@ -103,12 +103,13 @@ namespace RavenFS.Synchronization
 						return;
 					}
 
-					// if there is a work of the same type but for lower file ETag just refresh work metadata
-					if (pendingWork.SynchronizationType == workItem.SynchronizationType &&
+					// if there is a work for a file of the same type but with lower file ETag just refresh existing work metadata and do not enqueue again
+					if (pendingWork.FileName == workItem.FileName &&
+						pendingWork.SynchronizationType == workItem.SynchronizationType &&
 						Buffers.Compare(workItem.FileETag.ToByteArray(), pendingWork.FileETag.ToByteArray()) > 0)
 					{
 						pendingWork.RefreshMetadata();
-						log.Debug("{0} for a file {1} and a destination {2} was already existed in a pending queue byt with older ETag, it's metadata has been refreshed", workItem.GetType().Name, workItem.FileName, destination);
+						log.Debug("{0} for a file {1} and a destination {2} was already existed in a pending queue but with older ETag, it's metadata has been refreshed", workItem.GetType().Name, workItem.FileName, destination);
 						return;
 					}
 				}
