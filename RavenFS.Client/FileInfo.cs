8aaa92a70894d5de307d82b73c33f09a3cb2e935
Arkadiusz Palinski <arek@ais.pl>
2012-08-08 13:34:18 +0200

    Fixing serialization exception because of not nullable type

--- RavenFS.Client/FileInfo.cs
+++ RavenFS.Client/FileInfo.cs
@@ -6,7 +6,7 @@ namespace RavenFS.Client
 	public class FileInfo
 	{
 		public string Name { get; set; }
-		public long TotalSize { get; set; }
+		public long? TotalSize { get; set; }
 		public string HumaneTotalSize { get; set; }
 		public NameValueCollection Metadata { get; set; }
 	}
