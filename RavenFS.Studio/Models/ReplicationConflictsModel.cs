ab7a82e70ac8190b2fbac70292ff28b3e5e45195
samuel_d_jack <sam@seaturtlesoftware.com>
2012-08-24 11:02:53 +0100

    Added a conflicts view

---
+++ RavenFS.Studio/Models/ReplicationConflictsModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using RavenFS.Studio.Infrastructure;
+
+namespace RavenFS.Studio.Models
+{
+    public class ReplicationConflictsModel : ViewModel
+    {
+        public VirtualCollectionSource<FileSystemModel> ConflictedFiles { get; private set; }
+
+        public ReplicationConflictsModel()
+        {
+            ConflictedFiles = new SearchResultsCollectionSource()
+                {SearchPattern = "Raven-Synchronization-Conflict:True"};
+        }
+
+        protected override void OnViewLoaded()
+        {
+            ConflictedFiles.Refresh(RefreshMode.ClearStaleData);
+
+            ApplicationModel.Current.Client.Notifications.ConflictDetected()
+                .Throttle(TimeSpan.FromSeconds(1))
+                .TakeUntil(Unloaded)
+                .ObserveOnDispatcher()
+                .Subscribe(notification => ConflictedFiles.Refresh(RefreshMode.PermitStaleDataWhilstRefreshing));
+        }
+    }
+}
