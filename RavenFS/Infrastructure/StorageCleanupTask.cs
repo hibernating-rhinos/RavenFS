8b6f16cb9f4e6b62139e81334edada8caf79b1fd
Arkadiusz Palinski <arek@ais.pl>
2012-09-25 13:07:16 +0200

    Make delete operation aware of concurrency. Logging added

--- RavenFS/Infrastructure/StorageCleanupTask.cs
+++ RavenFS/Infrastructure/StorageCleanupTask.cs
@@ -38,49 +38,61 @@
 			var deletingFileName = RavenFileNameHelper.DeletingFileName(fileName);
 			var fileExists = true;
 
+			ConcurrencyAwareExecutor.Execute(() =>
+			    storage.Batch(accessor =>
+			    {
+				    FileAndPages fileAndPages;
 
-			storage.Batch(accessor =>
-			{
-				FileAndPages fileAndPages;
+				    try
+				    {
+					    fileAndPages = accessor.GetFile(fileName, 0, 0);
+				    }
+				    catch (FileNotFoundException)
+				    {
+					    // do nothing if file does not exist
+					    fileExists = false;
+					    return;
+				    }
 
-				try
-				{
-					fileAndPages = accessor.GetFile(fileName, 0, 0);
-				}
-				catch (FileNotFoundException)
-				{
-					// do nothing if file does not exist
-					fileExists = false;
-					return;
-				}
+				    var metadata = new NameValueCollection(fileAndPages.Metadata)
+					                {
+						                {SynchronizationConstants.RavenDeleteMarker, "true"}
+					                };
 
-				var metadata = new NameValueCollection(fileAndPages.Metadata)
-					               {
-						               {SynchronizationConstants.RavenDeleteMarker, "true"}
-					               };
+				    var renameSucceeded = false;
 
-				var renameSucceeded = false;
+				    var deleteVersion = 0;
 
-				var deleteVersion = 0;
+				    do
+				    {
+					    try
+					    {
+						    accessor.RenameFile(fileName, deletingFileName);
+						    renameSucceeded = true;
+					    }
+					    catch (EsentKeyDuplicateException)
+					    {
+						    // it means that .deleting file was already existed
+						    // we need to use different name to do a file rename
+						    deleteVersion++;
+						    deletingFileName = RavenFileNameHelper.DeletingFileName(fileName, deleteVersion);
+					    }
+				    } while (!renameSucceeded && deleteVersion < 128);
 
-				do
-				{
-					try
-					{
-						accessor.RenameFile(fileName, deletingFileName);
-						renameSucceeded = true;
-					}
-					catch (EsentKeyDuplicateException)
-					{
-						// it means that .deleting file was already existed
-						// we need to use different name to do a file rename
-						deleteVersion++;
-						deletingFileName = RavenFileNameHelper.DeletingFileName(fileName, deleteVersion);
-					}
-				} while (!renameSucceeded && deleteVersion < 128);
-				
-				accessor.UpdateFileMetadata(deletingFileName, metadata);
-			});
+				    if (renameSucceeded)
+				    {
+					    accessor.UpdateFileMetadata(deletingFileName, metadata);
+
+					    log.Debug(string.Format("File '{0}' was renamed to '{1}' and marked as deleted",
+					                            fileName, deletingFileName));
+				    }
+				    else
+				    {
+					    log.Warn("Could not rename a file '{0}' when a delete operation was performed",
+					            fileName);
+				    }
+			    })
+			);
 
 			if (fileExists)
 			{
