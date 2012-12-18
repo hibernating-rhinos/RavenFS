08ce31993f23f23509602b66f09af6d79139c5ff
samuel_d_jack <sam@seaturtlesoftware.com>
2012-12-18 11:40:55 +0000

    Added an EditConfiguration page

--- RavenFS.Studio/Models/ConfigurationPageModel.cs
+++ RavenFS.Studio/Models/ConfigurationPageModel.cs
@@ -26,27 +26,24 @@ namespace RavenFS.Studio.Models
 {
     public class ConfigurationPageModel : PageModel
     {
-        public ObservableCollection<ConfigurationModel> AvailableConfigurations { get; private set; }
-        public Observable<ConfigurationModel> SelectedConfiguration { get; private set; } 
-        public Observable<NameValueCollectionEditorModel> ConfigurationSettings { get; private set; }
-
         private ICommand addNewConfiguration;
         private ICommand saveConfiguration;
         private ICommand deleteConfiguration;
-        private ICommand discardChanges;
+        private ICommand editConfiguration;
 
-        public ICommand AddNewConfiguration { get { return addNewConfiguration ?? new ActionCommand(HandleAddNewConfiguration);  } }
+        public ICommand AddNewConfiguration { get { return addNewConfiguration ?? (addNewConfiguration = new ActionCommand(HandleAddNewConfiguration));  } }
 
-        public ICommand SaveConfiguration
+        public ICommand EditConfiguration { get { return editConfiguration ?? (editConfiguration = new ActionCommand(HandleEditConfiguration)); } }
+
+        private void HandleEditConfiguration(object parameter)
         {
-            get
+            var configuration = parameter as ConfigurationModel;
+            if (configuration == null)
             {
-                return saveConfiguration
-                       ??
-                       new ActionObservableDependantCommand<ConfigurationModel>(SelectedConfiguration,
-                                                                                HandleSaveConfiguration,
-                                                                                configuration => configuration != null && configuration.IsModified);
+                return;
             }
+
+            UrlUtil.Navigate("/EditConfiguration?name=" + configuration.Name);
         }
 
         public ICommand DeleteConfiguration
@@ -60,66 +57,19 @@ namespace RavenFS.Studio.Models
             }
         }
 
-        public ICommand DiscardChanges
-        {
-            get
-            {
-                return discardChanges
-                       ?? new ActionObservableDependantCommand<ConfigurationModel>(SelectedConfiguration,
-                                                                                   HandleDiscardChanges,
-                                                                                   configuration => configuration != null && configuration.IsModified);
-            }
-        }
+       
 
         public ConfigurationPageModel()
         {
-            AvailableConfigurations = new ObservableCollection<ConfigurationModel>();
-
-            SelectedConfiguration = new Observable<ConfigurationModel>() { Value = null };
-            SelectedConfiguration.PropertyChanged += HandleSelectedConfigurationChanged;
-
-            ConfigurationSettings = new Observable<NameValueCollectionEditorModel>();
-        }
-
-        private void HandleDiscardChanges(ConfigurationModel configuration)
-        {
-            AskUser.ConfirmationAsync(
-                "Discard Changes",
-                string.Format("Are you sure you want to discard the changes you have made to configuration '{0}'",configuration.Name))
-                .ContinueWhenTrueInTheUIThread(() =>
-                                                   {
-                                                       ApplicationModel.Current.State.ModifiedConfigurations.Remove(
-                                                           configuration.Name);
-                                                       configuration.IsModified = false;
-                                                       BeginEditConfiguration(configuration);
-                                                   });
-
 
         }
 
         private void HandleAddNewConfiguration()
         {
-            AskUser.QuestionAsync("Create Configuration", "Name")
-                .ContinueOnUIThread(t =>
-                                        {
-                                            if (!t.IsCanceled)
-                                            {
-                                                var newName = t.Result;
-                                                ApplicationModel.Current.State.ModifiedConfigurations.Add(newName, new NameValueCollection());
-                                                BeginLoadConfigurations().ContinueOnSuccessInTheUIThread(() => SelectedConfiguration.Value = AvailableConfigurations.FirstOrDefault(c => c.Name.Equals(newName)));
-                                            }
-                                        });
+            UrlUtil.Navigate("/EditConfiguration");
         }
 
-        private void HandleSaveConfiguration(ConfigurationModel configuration)
-        {
-            ApplicationModel.Current.AsyncOperations.Do(
-                () =>
-                SaveConfigurationAsync(configuration,
-                                       ApplicationModel.Current.State.ModifiedConfigurations[configuration.Name]),
-                string.Format("Saving configuration '{0}'", configuration));
-        }
-
+       
         private void HandleDeleteConfiguration(ConfigurationModel configuration)
         {
             AskUser.ConfirmationAsync(
@@ -159,45 +109,7 @@ namespace RavenFS.Studio.Models
             BeginEditConfiguration(SelectedConfiguration.Value);
         }
 
-        private void BeginEditConfiguration(ConfigurationModel configuration)
-        {
-            if (configuration != null)
-            {
-                if (ApplicationModel.Current.State.ModifiedConfigurations.ContainsKey(configuration.Name))
-                {
-                    EditConfigurationValues(configuration,
-                                            ApplicationModel.Current.State.ModifiedConfigurations[configuration.Name]);
-                }
-                else
-                {
-                    ConfigurationSettings.Value = null;
-                    ApplicationModel.Current.Client.Config.GetConfig(configuration.Name)
-                        .ContinueOnUIThread(t => EditConfigurationValues(configuration, t.Result));
-                }
-            }
-            else
-            {
-                ConfigurationSettings.Value = null;
-            }
-        }
-
-        private void EditConfigurationValues(ConfigurationModel currentConfiguration, NameValueCollection settings)
-        {
-            if (SelectedConfiguration.Value != currentConfiguration)
-            {
-                return;
-            }
-
-            var editor = new NameValueCollectionEditorModel(settings);
-            editor.Changed += delegate
-                                                 {
-                                                     ApplicationModel.Current
-                                                         .State.ModifiedConfigurations[currentConfiguration.Name] = editor.GetCurrent();
-                                                     currentConfiguration.IsModified = true;
-                                                 };
-            ConfigurationSettings.Value = editor;
-        }
-
+      
 
         protected override void OnViewLoaded()
         {
---
+++ RavenFS.Studio/Models/EditConfigurationModel.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using RavenFS.Client;
+using RavenFS.Studio.Commands;
+using RavenFS.Studio.Extensions;
+using RavenFS.Studio.Infrastructure;
+using RavenFS.Studio.Infrastructure.Input;
+
+namespace RavenFS.Studio.Models
+{
+    public class EditConfigurationModel : PageModel
+    {
+        private ICommand _saveCommand;
+        private ICommand _deleteCommand;
+        private ICommand _refreshCommand;
+        public Observable<NameValueCollectionEditorModel> ConfigurationSettings { get; private set; }
+        public Observable<string> Name { get; private set; }
+        public Observable<string> OriginalName { get; private set; }
+        private bool hasUnsavedChanges;
+
+        public EditConfigurationModel()
+        {
+            ConfigurationSettings = new Observable<NameValueCollectionEditorModel>();
+            Name = new Observable<string>();
+            OriginalName = new Observable<string>();
+        }
+
+        protected override void OnViewLoaded()
+        {
+            base.OnViewLoaded();
+
+            var configName = QueryParameters.GetValueOrDefault("name", "");
+
+            if (!configName.IsNullOrEmpty())
+            {
+                BeginLoadConfig(configName);
+                OriginalName.Value = configName;
+                Name.Value = configName;
+            }
+            else
+            {
+                OriginalName.Value = null;
+                Name.Value = "";
+                EditConfigurationValues(new NameValueCollection());
+            }
+        }
+
+        private void BeginLoadConfig(string configName)
+        {
+            ApplicationModel.Current.Client.Config.GetConfig(configName)
+                            .ContinueOnUIThread(t => EditConfigurationValues(t.Result));
+        }
+
+        private void EditConfigurationValues(NameValueCollection settings)
+        {
+            hasUnsavedChanges = false;
+            var editor = new NameValueCollectionEditorModel(settings);
+            editor.Changed += delegate
+                {
+                    hasUnsavedChanges = true;
+                };
+
+            ConfigurationSettings.Value = editor;
+        }
+
+        public ICommand SaveCommand
+        {
+            get { return _saveCommand ?? (_saveCommand = new ActionCommand(HandleSave)); }
+        }
+
+        public ICommand DeleteCommand
+        {
+            get { return _deleteCommand ?? (_deleteCommand = new ActionObservableDependantCommand<string>(OriginalName, HandleDelete, name => !name.IsNullOrEmpty())); }
+        }
+
+        public ICommand RefreshCommand
+        {
+            get { return _refreshCommand ?? (_refreshCommand = new ActionObservableDependantCommand<string>(OriginalName, HandleRefresh, name => !name.IsNullOrEmpty())); }
+        }
+
+        private void HandleSave()
+        {
+            var configurationName = Name.Value;
+            var configValues = ConfigurationSettings.Value.GetCurrent();
+
+            if (configurationName.IsNullOrEmpty())
+            {
+                AskUser.AlertUser("Save Configuration", "Please enter a name for the configuration.");
+                return;
+            }
+
+            ApplicationModel.Current.AsyncOperations.Do(
+               () =>ApplicationModel.Current.Client.Config.SetConfig(configurationName, configValues)
+                   .ContinueOnSuccessInTheUIThread(() => OriginalName.Value = configurationName),
+            string.Format("Saving configuration '{0}'", configurationName));            
+        }
+
+        private void HandleDelete(string configurationName)
+        {
+            AskUser.ConfirmationAsync(
+                "Delete Configuration",
+                string.Format("Are you sure you want to delete configuration '{0}'?", configurationName))
+                .ContinueWhenTrueInTheUIThread(
+                    () =>
+                        {
+                            ApplicationModel.Current.AsyncOperations.Do(
+                                () => ApplicationModel.Current.Client.Config.DeleteConfig(configurationName),
+                                string.Format("Deleting configuration '{0}'", configurationName));
+                            UrlUtil.Navigate("/Configuration");
+                        });
+        }
+
+        private void HandleRefresh(string configurationName)
+        {
+            if (hasUnsavedChanges)
+            {
+                AskUser.ConfirmationAsync(
+                    "Refresh Configuration",
+                    string.Format("Are you sure you want to discard your changes to '{0}'?", configurationName))
+                       .ContinueWhenTrueInTheUIThread(
+                           () => BeginLoadConfig(configurationName));
+            }
+            else
+            {
+                BeginLoadConfig(configurationName);
+            }
+        }
+    }
+}
