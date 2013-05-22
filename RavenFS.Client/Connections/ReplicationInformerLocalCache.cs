f67a111980f7ab2b73ece77807c4b1b1023a20d9
Daniel <daniel@ayende.com>
2013-05-22 15:59:51 +0300

    Start adding FailoverBehavior

---
+++ RavenFS.Client/Connections/ReplicationInformerLocalCache.cs
@@ -0,0 +1,75 @@
+﻿#if!NETFX_CORE
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace RavenFS.Client.Connections
+{
+	public static class ReplicationInformerLocalCache
+	{
+		private readonly static Logger Log = LogManager.GetCurrentClassLogger();
+
+		public static IsolatedStorageFile GetIsolatedStorageFileForReplicationInformation()
+		{
+#if SILVERLIGHT
+			return IsolatedStorageFile.GetUserStoreForSite();
+#elif MONO
+			return IsolatedStorageFile.GetUserStoreForApplication();
+#else
+			return IsolatedStorageFile.GetMachineStoreForDomain();
+#endif
+		}
+
+		public static List<string> TryLoadReplicationInformationFromLocalCache(string serverHash)
+		{
+			try
+			{
+				using (var machineStoreForApplication = GetIsolatedStorageFileForReplicationInformation())
+				{
+					var path = "RavenDB Replication Information For - " + serverHash;
+
+					if (machineStoreForApplication.GetFileNames(path).Length == 0)
+						return null;
+
+					using (var stream = new IsolatedStorageFileStream(path, FileMode.Open, machineStoreForApplication))
+					{
+						var buffer = new byte[stream.Length];
+						var bytes = stream.Read(buffer, 0, (int) stream.Length);
+						return Encoding.Default.GetString(buffer,0, bytes).Split(' ').ToList();
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Log.ErrorException("Could not understand the persisted replication information", e);
+				return null;
+			}
+		}
+
+		public static void TrySavingReplicationInformationToLocalCache(string serverHash, List<string> destinations)
+		{
+			try
+			{
+				using (var machineStoreForApplication = GetIsolatedStorageFileForReplicationInformation())
+				{
+					var path = "RavenDB Replication Information For - " + serverHash;
+					using (var stream = new IsolatedStorageFileStream(path, FileMode.Create, machineStoreForApplication))
+					{
+						var data = string.Join(" ", destinations);
+						var bytes = Encoding.UTF8.GetBytes(data);
+						stream.Write(bytes,0, bytes.Length);
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Log.ErrorException("Could not persist the replication information", e);
+			}
+		}
+	}
+}
+#endif
---
+++ RavenFS.Client/Util/SystemTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RavenFS.Client.Util
+{
+	public static class SystemTime
+	{
+		public static Func<DateTime> UtcDateTime;
+
+		public static DateTime UtcNow
+		{
+			get
+			{
+				var temp = UtcDateTime;
+				return temp == null ? DateTime.UtcNow : temp();
+			}
+		}
+	}
+}
